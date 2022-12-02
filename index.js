9dcf2a7fdf9250ba50b1b5ea24165120f7790623
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-02 08:48:52 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+// Code your solution in this file!const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+  return arrayOfDrivers.slice(0, 2);
+};
+const returnLastTwoDrivers = function (array) {
+  return array.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function (multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+};
+const fareDoubler = createFareMultiplier(2)
+const fareTripler  = createFareMultiplier(3)
+// console.log(fareDoubler, fareTripler);  //Function anonymus
+const selectDifferentDrivers = function (array, selector) {
+    return selector(array);
+  };
